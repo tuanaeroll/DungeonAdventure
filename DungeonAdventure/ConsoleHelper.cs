f1db901fbbb26655c00a4f51e7295b5f698a7814
tuanaeroll <erolltuana@gmail.com>
2025-01-13 15:38:13 +0300

    added

---
+++ DungeonAdventure/ConsoleHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonAdventure
+{
+    public static class ConsoleHelper
+    {
+        public static void WriteLineColored(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        public static void WriteColored(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+    }
+}
--- DungeonAdventure/obj/Debug/net9.0/DungeonAdventure.AssemblyInfo.cs
+++ DungeonAdventure/obj/Debug/net9.0/DungeonAdventure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonAdventure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92ad986322d9eda39716cb7cd30f4955043446d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonAdventure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonAdventure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
